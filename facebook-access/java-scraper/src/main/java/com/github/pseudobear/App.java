8bbeb245a01143b4a206f1bf250d198a9e7db24f
pseudobear <2002michaelli@gmail.com>
2021-03-28 13:40:33 -0400

    rebuilt using maven

---
+++ facebook-access/java-scraper/src/main/java/com/github/pseudobear/App.java
@@ -0,0 +1,11 @@
+package com.github.pseudobear;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
--- java-scraper/src/java-scraper.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class JavaScraper {
-  public static void main(String[] args){
-    System.out.println("Running...");
-  }
-}
-
-
